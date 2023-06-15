eacffbf2bb1097a84acfc1af70324e4a8cc96f5d
SamadSRahman <121732577+SamadSRahman@users.noreply.github.com>
2023-06-15 11:53:03 +0530

    About us Added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import SideBar from './Sections/sideBar/SideBar';
 import YourData from './pages/yourData/YourData';
+import AboutUs from './pages/aboutUs/AboutUs';
 
 function App() {
   return (
@@ -15,12 +16,14 @@ function App() {
         <Header />
         <SideBar/>
         <Routes>
+
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
           <Route path='/' element={<DailyExp/>} />
           <Route path='/lastMonth' element={<LastMonth/>} />
           <Route path='/lastWeek' element={<LastWeek/>}/>
           <Route path='/yourdata' element={<YourData/>}/>
+          <Route path='/aboutus' element={<AboutUs/>}/>
         </Routes>
       </BrowserRouter>
      
